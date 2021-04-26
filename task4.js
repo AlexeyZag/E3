6f0b2e599a68da9d8019a71e2c94e16178115df2
AlexeyZag <72604307+AlexeyZag@users.noreply.github.com>
2021-04-26 08:54:52 +0300

    Add files via upload

---
+++ task4.js
@@ -0,0 +1,10 @@
+let a = 4
+let b = 10
+const intervalid = setInterval(function (){
+    console.log(a+=1)
+}, 1000)
+setTimeout(function (){
+    clearInterval(intervalid)
+}, (b - a + 2) * 1000)
+
+
